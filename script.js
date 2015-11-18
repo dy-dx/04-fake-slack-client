4abaff1e24fdfa6ad04f0de1e3b9492809c3d9b6
Chris Kim <christopherjkim@gmail.com>
2015-11-18 22:01:30 +0000

    changes

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function sendMessage (user, content, callback) {
 }
 
 function getMessages (callback) {
-  $.get(messagesRoute)
+  $.get('https://node-slack-christopherjkim.c9.io/messages')
   .done(callback);
 }
 
@@ -17,12 +17,35 @@ $('#submit-message').submit(function (e) {
   e.preventDefault();
 
   /* TODO: send message to server */
+  var username = $('#submit-user').val();
+  var content = $('#submit-content').val();
 
+  sendMessage(username, content);
 });
 
+function formatMessage(message) {
 
-/* TODO: use setInterval to periodically get new messages and update the list */
+  var avatarUrl = 'http://static.dnaindia.com/sites/default/files/2015/10/02/361285-john-cena-2.jpg';
+  var avatar = '<img src="' + avatarUrl + '" height="100px" width="100px"></img>';
+  var millis = message.timestamp;
+  var mins = Math.floor((millis % 36e5) / 6e4),
+    secs = Math.floor((millis % 6e4) / 1000);
+  var timeString = mins + ':' + secs;
+  return '<li>' + avatar + '<span class="timestamp">' + timeString + '</span> ' + '<strong>' + message.user + '</strong>' + ': ' + message.content + '</li>';
+}
 
-getMessages(function (messages) {
-  console.log(messages);
-});
+/* use setInterval to periodically get new messages and update the list */
+function updateMessages() {
+  var getMessagesCallback = function (messages) {
+    // empty message log
+    $('.messages').empty();
+
+    for (var i = 0; i < messages.length; i++) {
+      var message = messages[i];
+      $('.messages').append(formatMessage(message));
+    }
+  };
+
+  getMessages(getMessagesCallback);
+}
+window.setInterval(updateMessages, 50);
