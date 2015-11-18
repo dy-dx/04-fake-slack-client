84802d1073db62d0fe5f771ed0aeb26983d5a13d
Chris Kim <christopherjkim@gmail.com>
2015-11-18 15:02:22 -0500

    add more starter code

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('hello world!');
+var host = 'https://node-slack-christopherjkim.c9.io';
+var sendRoute = host + '/send';
+var messagesRoute = host + '/messages';
+
+function sendMessage (user, content, callback) {
+  $.post(sendRoute, {user: user, content: content})
+  .done(callback);
+}
+
+function getMessages (callback) {
+  $.get(messagesRoute)
+  .done(callback);
+}
+
+
+$('#submit-message').submit(function (e) {
+  e.preventDefault();
+
+  /* TODO: send message to server */
+
+});
+
+
+/* TODO: use setInterval to periodically get new messages and update the list */
+
+getMessages(function (messages) {
+  console.log(messages);
+});
